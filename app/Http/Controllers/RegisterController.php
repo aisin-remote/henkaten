d6bb6622868f5dca69ce649c972c2ac82bfba2f2
naufalamr17 <naufal.amr17@outlook.co.id>
2023-09-26 13:47:05 +0700

    Add: Login/Register Feature

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -3,11 +3,35 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use App\Models\User;
 
 class RegisterController extends Controller
 {
     public function index()
     {
         return view('pages.auth.register');
+    }
+
+    public function store(Request $request)
+    {
+        // Validasi data yang diterima dari formulir pendaftaran
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'npk' => 'required|string|max:255|unique:users', // Pastikan npk adalah unik dalam tabel users
+            'password' => 'required|string|min:6|confirmed', // 'confirmed' memastikan cocok dengan konfirmasi password
+            'password_confirmation' => 'required|string|min:6|same:password',
+        ]);
+
+        // Simpan data ke database
+        $user = new User();
+        $user->id = Str::uuid(); // Generate UUID sebagai ID
+        $user->name = $request->input('name');
+        $user->npk = $request->input('npk');
+        $user->password = bcrypt($request->input('password')); // Mengenkripsi password
+        $user->save();
+
+        // Redirect ke halaman login atau sesuaikan dengan halaman yang Anda inginkan
+        return redirect("/login")->with('success', 'Registration successful. You can now login.');
     }
 }
--- resources/views/pages/auth/login.blade.php
+++ resources/views/pages/auth/login.blade.php
@@ -1,25 +1,31 @@
 @extends('layouts.root.auth')
 
 @section('main')
-    <div class="authentication-login min-vh-100 bg-body row justify-content-center align-items-center p-4">
-        <div class="col-sm-8 col-md-6 col-xl-9">
-            <h2 class="mb-3 fs-7 fw-bolder">Digital Henkaten</h2>
-            <p class=" mb-9">Your Admin Dashboard</p>
-            <form>
-                <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">Username</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                </div>
-                <div class="mb-4">
-                    <label for="exampleInputPassword1" class="form-label">Password</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1">
-                </div>
-                <a href="index.html" class="btn btn-primary w-100 py-8 mb-4 rounded-2 fw-medium">Sign In</a>
-                <div class="d-flex align-items-center justify-content-center">
-                    <small class="fs-4 mb-0 fw-medium">New to Henkaten?</small>
-                    <a class="text-primary fw-medium ms-2" href="{{ route('register.index') }}">Create an account</a>
-                </div>
-            </form>
+<div class="authentication-login min-vh-100 bg-body row justify-content-center align-items-center p-4">
+    <div class="col-sm-8 col-md-6 col-xl-9">
+        @if(session('error'))
+        <div class="alert alert-danger">
+            {{ session('error') }}
         </div>
+        @endif
+        <h2 class="mb-3 fs-7 fw-bolder">Digital Henkaten</h2>
+        <p class="mb-9">Your Admin Dashboard</p>
+        <form method="POST" action="{{ route('login.login') }}">
+            @csrf
+            <div class="mb-3">
+                <label for="npk" class="form-label">NPK</label>
+                <input type="text" class="form-control" id="npk" name="npk" required>
+            </div>
+            <div class="mb-4">
+                <label for="password" class="form-label">Password</label>
+                <input type="password" class="form-control" id="password" name="password" required>
+            </div>
+            <button type="submit" class="btn btn-primary w-100 py-8 mb-4 rounded-2 fw-medium">Sign In</button>
+            <div class="d-flex align-items-center justify-content-center">
+                <small class="fs-4 mb-0 fw-medium">New to Henkaten?</small>
+                <a class="text-primary fw-medium ms-2" href="{{ route('register.index') }}">Create an account</a>
+            </div>
+        </form>
     </div>
+</div>
 @endsection
