a57ac048d5dd848c5f5309ddb49f2d0885761331
naufalamr17 <naufal.amr17@outlook.co.id>
2023-09-27 13:25:31 +0700

    Add: Try Vertical Carousel

--- resources/views/layouts/root/main.blade.php
+++ resources/views/layouts/root/main.blade.php
@@ -16,24 +16,44 @@
     <meta name="keywords" content="Mordenize" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <!-- Favicon -->
-    <link rel="shortcut icon" type="image/png"
-        href="https://demos.adminmart.com/premium/bootstrap/modernize-bootstrap/package/dist/images/logos/favicon.ico" />
+    <link rel="shortcut icon" type="image/png" href="https://demos.adminmart.com/premium/bootstrap/modernize-bootstrap/package/dist/images/logos/favicon.ico" />
     <!-- Owl Carousel -->
     <link rel="stylesheet" href={{ asset('dist/libs/owl.carousel/dist/assets/owl.carousel.min.css') }}>
     <link rel="stylesheet" href={{ asset('dist/libs/select2/dist/css/select2.min.css') }}>
     <!-- Core Css -->
     <link id="themeColors" rel="stylesheet" href={{ asset('dist/css/style.min.css') }} />
+
+    <style>
+        .vertical-carousel {
+            overflow: hidden;
+            height: 300px;
+            /* Sesuaikan tinggi carousel dengan kebutuhan Anda */
+            position: relative;
+            display: flex;
+            flex-direction: column;
+            /* Carousel vertikal */
+        }
+
+        .carousel-item {
+            width: 100%;
+            flex: 1;
+            /* Mengisi tinggi yang tersedia dalam carousel */
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            font-size: 24px;
+            transition: transform 0.5s ease-in-out;
+        }
+    </style>
 </head>
 
 <body>
     <!-- Preloader -->
     <div class="preloader">
-        <img src="https://demos.adminmart.com/premium/bootstrap/modernize-bootstrap/package/dist/images/logos/favicon.ico"
-            alt="loader" class="lds-ripple img-fluid" />
+        <img src="https://demos.adminmart.com/premium/bootstrap/modernize-bootstrap/package/dist/images/logos/favicon.ico" alt="loader" class="lds-ripple img-fluid" />
     </div>
     <!-- Body Wrapper -->
-    <div class="page-wrapper" id="main-wrapper" data-layout="horizontal" data-navbarbg="skin6" data-sidebartype="full"
-        data-sidebar-position="fixed" data-header-position="fixed">
+    <div class="page-wrapper" id="main-wrapper" data-layout="horizontal" data-navbarbg="skin6" data-sidebartype="full" data-sidebar-position="fixed" data-header-position="fixed">
         <!-- Header Start -->
         @include('layouts.partials.header')
 
@@ -94,8 +114,7 @@
                         <li class="p-1 mb-1 bg-hover-light-black">
                             <a href="#">
                                 <span class="fs-3 text-black fw-normal d-block">Detail</span>
-                                <span
-                                    class="fs-3 text-muted d-block">/apps/blog/detail/streaming-video-way-before-it-was-cool-go-dark-tomorrow</span>
+                                <span class="fs-3 text-muted d-block">/apps/blog/detail/streaming-video-way-before-it-was-cool-go-dark-tomorrow</span>
                             </a>
                         </li>
                         <li class="p-1 mb-1 bg-hover-light-black">
@@ -131,8 +150,7 @@
                         <li class="p-1 mb-1 bg-hover-light-black">
                             <a href="#">
                                 <span class="fs-3 text-black fw-normal d-block">Detail</span>
-                                <span
-                                    class="fs-3 text-muted d-block">/apps/blog/detail/streaming-video-way-before-it-was-cool-go-dark-tomorrow</span>
+                                <span class="fs-3 text-muted d-block">/apps/blog/detail/streaming-video-way-before-it-was-cool-go-dark-tomorrow</span>
                             </a>
                         </li>
                         <li class="p-1 mb-1 bg-hover-light-black">
@@ -164,6 +182,35 @@
     <script src={{ asset('dist/libs/select2/dist/js/select2.full.min.js') }}></script>
     {{-- <script src={{ asset('dist/libs/select2/dist/js/select2.min.js') }}></script> --}}
     <script src={{ asset('dist/js/forms/select2.init.js') }}></script>
+
+    <script>
+        const carousel = document.querySelector('.vertical-carousel');
+        const items = carousel.querySelectorAll('.carousel-item');
+        let currentIndex = 0;
+
+        function nextItem() {
+            currentIndex = (currentIndex + 1) % items.length;
+            updateCarousel();
+        }
+
+        function prevItem() {
+            currentIndex = (currentIndex - 1 + items.length) % items.length;
+            updateCarousel();
+        }
+
+        function updateCarousel() {
+            const translateY = -currentIndex * 100;
+            items.forEach((item, index) => {
+                item.style.transform = `translateY(${translateY}%)`;
+            });
+        }
+
+        // Trigger next item on click (you can add buttons for this)
+        carousel.addEventListener('click', nextItem);
+
+        // Optional: Auto-rotate the carousel
+         setInterval(nextItem, 3000); // Uncomment this line to auto-rotate every 3 seconds
+    </script>
 </body>
 
 <!-- Mirrored from demos.adminmart.com/premium/bootstrap/modernize-bootstrap/package/html/horizontal/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 26 Sep 2023 00:45:30 GMT -->
