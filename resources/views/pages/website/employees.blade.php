5eea7fa5db46dd04329f54b191a167abf998fceb
AIIA\fabian <fabian@aiia.co.id>
2023-11-02 09:35:59 +0700

    ubah dikt

--- resources/views/pages/website/employees.blade.php
+++ resources/views/pages/website/employees.blade.php
@@ -1,186 +1,64 @@
 @extends('layouts.root.main')
 
 @section('main')
-<div class="container-fluid">
-    <div class="owl-carousel counter-carousel owl-theme owl-rtl owl-loaded owl-drag">
-        <div class="owl-stage-outer">
-            <div class="owl-stage"
-                style="transform: translate3d(1238px, 0px, 0px); transition: all 2s ease 0s; width: 3713px;">
-                <div class="owl-item cloned" style="width: 279.4px; margin-left: 30px;">
-                    <div class="item">
-                        <div class="card border-0 zoom-in bg-light-danger shadow-none">
-                            <div class="card-body">
-                                <div class="text-center">
-                                    <img src="https://demos.adminmart.com/premium/bootstrap/modernize-bootstrap/package/dist/images/svgs/icon-favorites.svg"
-                                        width="50" height="50" class="mb-3" alt="">
-                                    <p class="fw-semibold fs-3 text-danger mb-1">Events</p>
-                                    <h5 class="fw-semibold text-danger mb-0">696</h5>
-                                </div>
+    <div class="col-md-12">
+        <div class="card">
+            <div class="card-body">
+                <!-- Nav tabs -->
+                <ul class="nav nav-pills nav-fill mt-4" role="tablist">
+                    <li class="nav-item" role="presentation">
+                        <a class="nav-link active" data-bs-toggle="tab" href="#navpill-111" role="tab"
+                            aria-selected="true">
+                            <span>Shift 1</span>
+                        </a>
+                    </li>
+                </ul>
+                <!-- Tab panes -->
+                <div class="tab-content border mt-2">
+                    <div class="tab-pane active p-3" id="navpill-111" role="tabpanel">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <p>
+                                    Raw denim you probably haven't heard of them jean
+                                    shorts Austin. Nesciunt tofu stumptown aliqua,
+                                    retro synth master cleanse. Mustache cliche
+                                    tempor, williamsburg carles vegan helvetica.
+                                </p>
                             </div>
                         </div>
                     </div>
-                </div>
-                <div class="owl-item cloned" style="width: 279.4px; margin-left: 30px;">
-                    <div class="item">
-                        <div class="card border-0 zoom-in bg-light-success shadow-none">
-                            <div class="card-body">
-                                <div class="text-center">
-                                    <img src="https://demos.adminmart.com/premium/bootstrap/modernize-bootstrap/package/dist/images/svgs/icon-speech-bubble.svg"
-                                        width="50" height="50" class="mb-3" alt="">
-                                    <p class="fw-semibold fs-3 text-success mb-1">Payroll</p>
-                                    <h5 class="fw-semibold text-success mb-0">$96k</h5>
-                                </div>
+                    <div class="tab-pane p-3" id="navpill-222" role="tabpanel">
+                        <div class="row">
+                            <div class="col-md-8">
+                                <p>
+                                    Raw denim you probably haven't heard of them jean
+                                    shorts Austin. Nesciunt tofu stumptown aliqua,
+                                    retro synth master cleanse. Mustache cliche
+                                    tempor, williamsburg carles vegan helvetica.
+                                </p>
+                            </div>
+                            <div class="col-md-4">
+                                <img src="../../dist/images/big/img1.jpg" alt="" class="img-fluid">
                             </div>
                         </div>
                     </div>
-                </div>
-                <div class="owl-item cloned" style="width: 279.4px; margin-left: 30px;">
-                    <div class="item">
-                        <div class="card border-0 zoom-in bg-light-info shadow-none">
-                            <div class="card-body">
-                                <div class="text-center">
-                                    <img src="https://demos.adminmart.com/premium/bootstrap/modernize-bootstrap/package/dist/images/svgs/icon-connect.svg"
-                                        width="50" height="50" class="mb-3" alt="">
-                                    <p class="fw-semibold fs-3 text-info mb-1">Reports</p>
-                                    <h5 class="fw-semibold text-info mb-0">59</h5>
-                                </div>
+                    <div class="tab-pane p-3" id="navpill-333" role="tabpanel">
+                        <div class="row">
+                            <div class="col-md-4">
+                                <img src="../../dist/images/big/img3.jpg" alt="" class="img-fluid">
                             </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="owl-item" style="width: 279.4px; margin-left: 30px;">
-                    <div class="item">
-                        <div class="card border-0 zoom-in bg-light-primary shadow-none">
-                            <div class="card-body">
-                                <div class="text-center">
-                                    <img src="https://demos.adminmart.com/premium/bootstrap/modernize-bootstrap/package/dist/images/svgs/icon-user-male.svg"
-                                        width="50" height="50" class="mb-3" alt="">
-                                    <p class="fw-semibold fs-3 text-primary mb-1"> Employees </p>
-                                    <h5 class="fw-semibold text-primary mb-0">96</h5>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="owl-item active" style="width: 279.4px; margin-left: 30px;">
-                    <div class="item">
-                        <div class="card border-0 zoom-in bg-light-warning shadow-none">
-                            <div class="card-body">
-                                <div class="text-center">
-                                    <img src="https://demos.adminmart.com/premium/bootstrap/modernize-bootstrap/package/dist/images/svgs/icon-briefcase.svg"
-                                        width="50" height="50" class="mb-3" alt="">
-                                    <p class="fw-semibold fs-3 text-warning mb-1">Clients</p>
-                                    <h5 class="fw-semibold text-warning mb-0">3,650</h5>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="owl-item active" style="width: 279.4px; margin-left: 30px;">
-                    <div class="item">
-                        <div class="card border-0 zoom-in bg-light-info shadow-none">
-                            <div class="card-body">
-                                <div class="text-center">
-                                    <img src="https://demos.adminmart.com/premium/bootstrap/modernize-bootstrap/package/dist/images/svgs/icon-mailbox.svg"
-                                        width="50" height="50" class="mb-3" alt="">
-                                    <p class="fw-semibold fs-3 text-info mb-1">Projects</p>
-                                    <h5 class="fw-semibold text-info mb-0">356</h5>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="owl-item active" style="width: 279.4px; margin-left: 30px;">
-                    <div class="item">
-                        <div class="card border-0 zoom-in bg-light-danger shadow-none">
-                            <div class="card-body">
-                                <div class="text-center">
-                                    <img src="https://demos.adminmart.com/premium/bootstrap/modernize-bootstrap/package/dist/images/svgs/icon-favorites.svg"
-                                        width="50" height="50" class="mb-3" alt="">
-                                    <p class="fw-semibold fs-3 text-danger mb-1">Events</p>
-                                    <h5 class="fw-semibold text-danger mb-0">696</h5>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="owl-item" style="width: 279.4px; margin-left: 30px;">
-                    <div class="item">
-                        <div class="card border-0 zoom-in bg-light-success shadow-none">
-                            <div class="card-body">
-                                <div class="text-center">
-                                    <img src="https://demos.adminmart.com/premium/bootstrap/modernize-bootstrap/package/dist/images/svgs/icon-speech-bubble.svg"
-                                        width="50" height="50" class="mb-3" alt="">
-                                    <p class="fw-semibold fs-3 text-success mb-1">Payroll</p>
-                                    <h5 class="fw-semibold text-success mb-0">$96k</h5>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="owl-item" style="width: 279.4px; margin-left: 30px;">
-                    <div class="item">
-                        <div class="card border-0 zoom-in bg-light-info shadow-none">
-                            <div class="card-body">
-                                <div class="text-center">
-                                    <img src="https://demos.adminmart.com/premium/bootstrap/modernize-bootstrap/package/dist/images/svgs/icon-connect.svg"
-                                        width="50" height="50" class="mb-3" alt="">
-                                    <p class="fw-semibold fs-3 text-info mb-1">Reports</p>
-                                    <h5 class="fw-semibold text-info mb-0">59</h5>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="owl-item cloned" style="width: 279.4px; margin-left: 30px;">
-                    <div class="item">
-                        <div class="card border-0 zoom-in bg-light-primary shadow-none">
-                            <div class="card-body">
-                                <div class="text-center">
-                                    <img src="https://demos.adminmart.com/premium/bootstrap/modernize-bootstrap/package/dist/images/svgs/icon-user-male.svg"
-                                        width="50" height="50" class="mb-3" alt="">
-                                    <p class="fw-semibold fs-3 text-primary mb-1"> Employees </p>
-                                    <h5 class="fw-semibold text-primary mb-0">96</h5>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="owl-item cloned" style="width: 279.4px; margin-left: 30px;">
-                    <div class="item">
-                        <div class="card border-0 zoom-in bg-light-warning shadow-none">
-                            <div class="card-body">
-                                <div class="text-center">
-                                    <img src="https://demos.adminmart.com/premium/bootstrap/modernize-bootstrap/package/dist/images/svgs/icon-briefcase.svg"
-                                        width="50" height="50" class="mb-3" alt="">
-                                    <p class="fw-semibold fs-3 text-warning mb-1">Clients</p>
-                                    <h5 class="fw-semibold text-warning mb-0">3,650</h5>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="owl-item cloned" style="width: 279.4px; margin-left: 30px;">
-                    <div class="item">
-                        <div class="card border-0 zoom-in bg-light-info shadow-none">
-                            <div class="card-body">
-                                <div class="text-center">
-                                    <img src="https://demos.adminmart.com/premium/bootstrap/modernize-bootstrap/package/dist/images/svgs/icon-mailbox.svg"
-                                        width="50" height="50" class="mb-3" alt="">
-                                    <p class="fw-semibold fs-3 text-info mb-1">Projects</p>
-                                    <h5 class="fw-semibold text-info mb-0">356</h5>
-                                </div>
+                            <div class="col-md-8">
+                                <p>
+                                    Raw denim you probably haven't heard of them jean
+                                    shorts Austin. Nesciunt tofu stumptown aliqua,
+                                    retro synth master cleanse. Mustache cliche
+                                    tempor, williamsburg carles vegan helvetica.
+                                </p>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="owl-nav disabled"><button type="button" role="presentation" class="owl-prev"><span
-                    aria-label="Previous">‹</span></button><button type="button" role="presentation"
-                class="owl-next"><span aria-label="Next">›</span></button></div>
-        <div class="owl-dots"><button role="button" class="owl-dot active"><span></span></button><button role="button"
-                class="owl-dot"><span></span></button><button role="button" class="owl-dot"><span></span></button></div>
     </div>
-</div>
 @endsection
