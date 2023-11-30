fe808009ccf2162a4330b0c33d92469a0c45ca79
AIIA\fabian <fabian@aiia.co.id>
2023-11-30 07:25:20 +0700

    fix update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,11 @@
 
 namespace App\Providers;
 
+use Carbon\Carbon;
 use App\Models\User;
+use App\Models\Henkaten;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -25,6 +28,25 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        View::composer('layouts.partials.header', function ($view) {
+
+            $currentDate = Carbon::now()->format('Y-m-d');
+            
+            // get all data from henaktens table that dont have troubleshoot
+            $henkatens = Henkaten::with('line')
+                        ->with('shift')
+                        ->where('is_done', '0')
+                        ->where('date', 'LIKE', '%' . $currentDate .'%')
+                        ->take(5)
+                        ->latest()
+                        ->get();
+
+            $view->with([
+                'henkatens' => $henkatens,
+                'henkatenItems' => count($henkatens)
+            ]);
+        });
+        
         Gate::define('JP', function(User $user){
             return $user->role === 'JP';
         });
--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -8,11 +8,13 @@
             </li>
             <li class="nav-item d-none d-xl-block">
                 <a href="#" class="text-nowrap nav-link">
-                    <img src="{{ asset('assets/images/henkaten-logo.svg') }}" class="dark-logo" width="180" alt="" />
+                    <img src="{{ asset('assets/images/henkaten-logo.svg') }}" class="dark-logo" width="180"
+                        alt="" />
                 </a>
             </li>
             <li class="nav-item d-none d-xl-block">
-                <a class="nav-link nav-icon-hover" href="javascript:void(0)" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                <a class="nav-link nav-icon-hover" href="javascript:void(0)" data-bs-toggle="modal"
+                    data-bs-target="#exampleModal">
                     <i class="ti ti-search"></i>
                 </a>
             </li>
@@ -22,106 +24,108 @@
         </ul>
         <div class="d-block d-xl-none">
             <a href="index.html" class="text-nowrap nav-link">
-                <img src="{{ asset('assets/images/henkaten-logo.svg') }}" class="dark-logo" width="180" alt="" />
+                <img src="{{ asset('assets/images/henkaten-logo.svg') }}" class="dark-logo" width="180"
+                    alt="" />
             </a>
         </div>
-        <button class="navbar-toggler p-0 border-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler p-0 border-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="p-2">
                 <i class="ti ti-dots fs-7"></i>
             </span>
         </button>
         <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
             <div class="d-flex align-items-center justify-content-between px-0 px-xl-8">
-                <a href="javascript:void(0)" class="nav-link round-40 p-1 ps-0 d-flex d-xl-none align-items-center justify-content-center" type="button" data-bs-toggle="offcanvas" data-bs-target="#mobilenavbar" aria-controls="offcanvasWithBothOptions">
+                <a href="javascript:void(0)"
+                    class="nav-link round-40 p-1 ps-0 d-flex d-xl-none align-items-center justify-content-center"
+                    type="button" data-bs-toggle="offcanvas" data-bs-target="#mobilenavbar"
+                    aria-controls="offcanvasWithBothOptions">
                     <i class="ti ti-align-justified fs-7"></i>
                 </a>
                 <ul class="navbar-nav flex-row ms-auto align-items-center justify-content-center">
                     <li class="nav-item dropdown">
-                        <a class="nav-link nav-icon-hover" href="javascript:void(0)" id="drop2" data-bs-toggle="dropdown" aria-expanded="false">
+                        <a class="nav-link nav-icon-hover" href="javascript:void(0)" id="drop2"
+                            data-bs-toggle="dropdown" aria-expanded="false">
                             <i class="ti ti-bell-ringing"></i>
-                            <div class="notification bg-primary rounded-circle"></div>
+                            @if (!$henkatens->isEmpty())
+                                <div class="notification bg-primary rounded-circle"></div>
+                            @endif
                         </a>
-                        <div class="dropdown-menu content-dd dropdown-menu-end dropdown-menu-animate-up" aria-labelledby="drop2">
+                        <div class="dropdown-menu content-dd dropdown-menu-end dropdown-menu-animate-up"
+                            aria-labelledby="drop2">
                             <div class="d-flex align-items-center justify-content-between py-3 px-7">
                                 <h5 class="mb-0 fs-5 fw-semibold">Notifications</h5>
-                                <span class="badge bg-primary rounded-4 px-3 py-1 lh-sm">5 new</span>
+                                @if (!$henkatens->isEmpty())
+                                    <span class="badge bg-primary rounded-4 px-3 py-1 lh-sm">{{ $henkatenItems }}
+                                        New</span>
+                                @endif
                             </div>
                             <div class="message-body" data-simplebar>
-                                <a href="javascript:void(0)" class="py-6 px-7 d-flex align-items-center dropdown-item">
-                                    <span class="me-3">
-                                        <img src="../../dist/images/profile/user-1.jpg" alt="user" class="rounded-circle" width="48" height="48" />
-                                    </span>
-                                    <div class="w-75 d-inline-block v-middle">
-                                        <h6 class="mb-1 fw-semibold">Roman Joined the Team!</h6>
-                                        <span class="d-block">Congratulate him</span>
-                                    </div>
-                                </a>
-                                <a href="javascript:void(0)" class="py-6 px-7 d-flex align-items-center dropdown-item">
-                                    <span class="me-3">
-                                        <img src="../../dist/images/profile/user-2.jpg" alt="user" class="rounded-circle" width="48" height="48" />
-                                    </span>
-                                    <div class="w-75 d-inline-block v-middle">
-                                        <h6 class="mb-1 fw-semibold">New message</h6>
-                                        <span class="d-block">Salma sent you new message</span>
-                                    </div>
-                                </a>
-                                <a href="javascript:void(0)" class="py-6 px-7 d-flex align-items-center dropdown-item">
-                                    <span class="me-3">
-                                        <img src="../../dist/images/profile/user-3.jpg" alt="user" class="rounded-circle" width="48" height="48" />
-                                    </span>
-                                    <div class="w-75 d-inline-block v-middle">
-                                        <h6 class="mb-1 fw-semibold">Bianca sent payment</h6>
-                                        <span class="d-block">Check your earnings</span>
-                                    </div>
-                                </a>
-                                <a href="javascript:void(0)" class="py-6 px-7 d-flex align-items-center dropdown-item">
-                                    <span class="me-3">
-                                        <img src="../../dist/images/profile/user-4.jpg" alt="user" class="rounded-circle" width="48" height="48" />
-                                    </span>
-                                    <div class="w-75 d-inline-block v-middle">
-                                        <h6 class="mb-1 fw-semibold">Jolly completed tasks</h6>
-                                        <span class="d-block">Assign her new tasks</span>
-                                    </div>
-                                </a>
-                                <a href="javascript:void(0)" class="py-6 px-7 d-flex align-items-center dropdown-item">
-                                    <span class="me-3">
-                                        <img src="../../dist/images/profile/user-5.jpg" alt="user" class="rounded-circle" width="48" height="48" />
-                                    </span>
-                                    <div class="w-75 d-inline-block v-middle">
-                                        <h6 class="mb-1 fw-semibold">John received payment</h6>
-                                        <span class="d-block">$230 deducted from account</span>
-                                    </div>
-                                </a>
-                                <a href="javascript:void(0)" class="py-6 px-7 d-flex align-items-center dropdown-item">
-                                    <span class="me-3">
-                                        <img src="../../dist/images/profile/user-1.jpg" alt="user" class="rounded-circle" width="48" height="48" />
-                                    </span>
-                                    <div class="w-75 d-inline-block v-middle">
-                                        <h6 class="mb-1 fw-semibold">Roman Joined the Team!</h6>
-                                        <span class="d-block">Congratulate him</span>
-                                    </div>
-                                </a>
+                                @if (!$henkatens->isEmpty())
+                                    @foreach ($henkatens as $henkaten)
+                                        @php
+                                            if ($henkaten->status == 'stop') {
+                                                $color = 'danger';
+                                                $shape = 'x';
+                                            } else {
+                                                $color = 'warning';
+                                                $shape = 'triangle';
+                                            }
+
+                                            if ($henkaten->troubleshoot === null) {
+                                                $text = 'Waiting Troubleshoot';
+                                                $colorStatus = 'danger';
+                                            } else {
+                                                $text = 'Waiting Approval';
+                                                $colorStatus = 'warning';
+                                            }
+                                        @endphp
+                                        <div class="d-flex align-items-center justify-content-between">
+                                            <a href="javascript:void(0)"
+                                                class="py-6 px-7 d-flex align-items-center dropdown-item">
+                                                <span class="badge bg-{{ $color }} me-3">
+                                                    <h3 class="text-white box mb-0 fw-bolder">
+                                                        <i class="ti ti-{{ $shape }}"></i>
+                                                    </h3>
+                                                </span>
+                                                <div class="w-75 d-inline-block v-middle">
+                                                    <h6 class="mb-1 fw-semibold">{{ $henkaten->abnormality }}</h6>
+                                                    <span class="d-block">{{ $henkaten->line->name }}</span>
+                                                </div>
+                                                <span class="badge bg-{{ $colorStatus }} rounded-4 py-1 lh-sm"
+                                                    style="font-size: 10px">{{ $text }}</span>
+                                            </a>
+                                        </div>
+                                    @endforeach
+                                @else
+                                    <p class="text-dark w-50 text-center mt-2" style="margin-left: 80px">No
+                                        Abnormality :)</p>
+                                @endif
                             </div>
                             <div class="py-6 px-7 mb-1">
-                                <button class="btn btn-outline-primary w-100"> See All Notifications </button>
+                                <button class="btn btn-outline-primary w-100"> See All Abnormality </button>
                             </div>
                         </div>
                     </li>
                     <li class="nav-item dropdown">
-                        <a class="nav-link pe-0" href="javascript:void(0)" id="drop1" data-bs-toggle="dropdown" aria-expanded="false">
+                        <a class="nav-link pe-0" href="javascript:void(0)" id="drop1" data-bs-toggle="dropdown"
+                            aria-expanded="false">
                             <div class="d-flex align-items-center">
                                 <div class="user-profile-img">
-                                    <img src="../../dist/images/profile/saiful.png" class="rounded-circle" width="35" height="35" alt="" />
+                                    <img src="../../dist/images/profile/saiful.png" class="rounded-circle"
+                                        width="35" height="35" alt="" />
                                 </div>
                             </div>
                         </a>
-                        <div class="dropdown-menu content-dd dropdown-menu-end dropdown-menu-animate-up" aria-labelledby="drop1">
+                        <div class="dropdown-menu content-dd dropdown-menu-end dropdown-menu-animate-up"
+                            aria-labelledby="drop1">
                             <div class="profile-dropdown position-relative" data-simplebar>
                                 <div class="py-3 px-7 pb-0">
                                     <h5 class="mb-0 fs-5 fw-semibold">User Profile</h5>
                                 </div>
                                 <div class="d-flex align-items-center py-9 mx-7 border-bottom">
-                                    <img src="../../dist/images/profile/saiful.png" class="rounded-circle" width="80" height="80" alt="" />
+                                    <img src="../../dist/images/profile/saiful.png" class="rounded-circle"
+                                        width="80" height="80" alt="" />
                                     <div class="ms-3">
                                         <h5 class="mb-1 fs-3">{{ auth()->user()->name }}</h5>
                                         <span class="mb-2 mt-2 d-block badge bg-light-info text-info">Leader</span>
@@ -132,34 +136,16 @@
                                 </div>
                                 <div class="message-body">
                                     <a href="page-user-profile.html" class="py-8 px-7 mt-8 d-flex align-items-center">
-                                        <span class="d-flex align-items-center justify-content-center bg-light rounded-1 p-6">
-                                            <img src="https://demos.adminmart.com/premium/bootstrap/modernize-bootstrap/package/dist/images/svgs/icon-account.svg" alt="" width="24" height="24">
+                                        <span
+                                            class="d-flex align-items-center justify-content-center bg-light rounded-1 p-6">
+                                            <img src="https://demos.adminmart.com/premium/bootstrap/modernize-bootstrap/package/dist/images/svgs/icon-account.svg"
+                                                alt="" width="24" height="24">
                                         </span>
                                         <div class="w-75 d-inline-block v-middle ps-3">
                                             <h6 class="mb-1 bg-hover-primary fw-semibold"> My Profile </h6>
                                             <span class="d-block text-dark">Account Settings</span>
                                         </div>
                                     </a>
-                                    <a href="app-email.html" class="py-8 px-7 d-flex align-items-center">
-                                        <span class="d-flex align-items-center justify-content-center bg-light rounded-1 p-6">
-                                            <img src="https://demos.adminmart.com/premium/bootstrap/modernize-bootstrap/package/dist/images/svgs/icon-inbox.svg" alt="" width="24" height="24">
-                                        </span>
-                                        <div class="w-75 d-inline-block v-middle ps-3">
-                                            <h6 class="mb-1 bg-hover-primary fw-semibold">My Inbox</h6>
-                                            <span class="d-block text-dark">Messages & Emails</span>
-                                        </div>
-                                    </a>
-                                    {{-- <a href="app-notes.html" class="py-8 px-7 d-flex align-items-center">
-                                        <span
-                                            class="d-flex align-items-center justify-content-center bg-light rounded-1 p-6">
-                                            <img src="https://demos.adminmart.com/premium/bootstrap/modernize-bootstrap/package/dist/images/svgs/icon-tasks.svg"
-                                                alt="" width="24" height="24">
-                                        </span>
-                                        <div class="w-75 d-inline-block v-middle ps-3">
-                                            <h6 class="mb-1 bg-hover-primary fw-semibold">My Task</h6>
-                                            <span class="d-block text-dark">To-do and Daily Tasks</span>
-                                        </div>
-                                    </a> --}}
                                 </div>
                                 <form action="{{ route('logout.auth') }}" method="post">
                                     @csrf
@@ -204,28 +190,27 @@
                 hours: 22,
                 minutes: 10,
                 seconds: 0
-            }            
+            }
         ];
 
         // Tentukan shift berdasarkan aturan jam
         var shift;
         for (var i = 0; i < shifts.length; i++) {
-        var shiftStartHour = shifts[i].hours;
-        var shiftEndHour = (i < shifts.length - 1) ? shifts[i + 1].hours : 24;
+            var shiftStartHour = shifts[i].hours;
+            var shiftEndHour = (i < shifts.length - 1) ? shifts[i + 1].hours : 24;
 
-        if (currentHour >= shiftStartHour && currentHour < shiftEndHour) {
-            shift = "Shift " + (i + 1);
-            break;
+            if (currentHour >= shiftStartHour && currentHour < shiftEndHour) {
+                shift = "Shift " + (i + 1);
+                break;
+            }
         }
-    }
 
-    // Jika tidak ada shift yang sesuai, tetapkan Shift 1 sebagai default
-    shift = shift || "Shift 1";
+        // Jika tidak ada shift yang sesuai, tetapkan Shift 1 sebagai default
+        shift = shift || "Shift 1";
 
-    // Tampilkan hasil shift
-    console.log(shift);
         // Perbarui nilai pada elemen shift
-        shiftElement.innerHTML = shift || "Shift 1"; // Jika tidak ada shift yang sesuai, tetapkan Shift 1 sebagai default
+        shiftElement.innerHTML = shift ||
+            "Shift 1"; // Jika tidak ada shift yang sesuai, tetapkan Shift 1 sebagai default
     }
 
     // Panggil fungsi updateShift saat halaman dimuat dan atur interval pembaruan (misalnya setiap 1 detik)
