215ed7db3a8c36e134578507852859da86dfb543
AIIA\fabian <fabian@aiia.co.id>
2024-01-08 09:01:57 +0700

    update scheduler to curl

--- app/Http/Controllers/HenkatenController.php
+++ app/Http/Controllers/HenkatenController.php
@@ -487,14 +487,24 @@ class HenkatenController extends Controller
 
     public function autoUpdate()
     {
-        $currentWeekStart = Carbon::now()->startOfWeek()->format('Y-m-d');
-        $currentWeekEnd = Carbon::now()->endOfWeek()->format('Y-m-d');
+        // Set the current time to the start of the current week, then subtract a week
+        $previousWeekStart = Carbon::now()
+            ->startOfWeek()
+            ->subWeek()
+            ->format('Y-m-d');
+            
+        // Set the time to the end of that week
+        $previousWeekEnd = Carbon::now()
+            ->startOfWeek()
+            ->subWeek()
+            ->endOfWeek()
+            ->format('Y-m-d');
 
         $currentShifts = EmployeeActive::select('employee_id', 'shift_id', 'line_id', 'pos_id')
-            ->where('active_from', '<=', $currentWeekStart)
-            ->where('expired_at', '>=', $currentWeekEnd)
+            ->where('active_from', '<=', $previousWeekEnd)
+            ->where('expired_at', '>=', $previousWeekStart)
             ->get();
-            
+
         $newShifts = $currentShifts->map(function ($employee) {
             $nextShiftUuid = $this->getNextShiftUuid($employee->shift_id); // Implement this method to determine the next shift
 
@@ -503,17 +513,21 @@ class HenkatenController extends Controller
                 'shift_id' => $nextShiftUuid,
                 'line_id' => $employee->line_id,
                 'pos_id' => $employee->pos_id,
-                'active_from' => Carbon::now()->startOfWeek()->format('Y-m-d'),
-                'expired_at' => Carbon::now()->endOfWeek()->format('Y-m-d'),
+                'active_from' => Carbon::now()
+                    ->startOfWeek()
+                    ->format('Y-m-d'),
+                'expired_at' => Carbon::now()
+                    ->endOfWeek()
+                    ->format('Y-m-d'),
             ];
         });
 
         $newShifts = $newShifts->toArray();
-        
+
         try {
             DB::beginTransaction();
-            
-            foreach($newShifts as $newShift) {
+
+            foreach ($newShifts as $newShift) {
                 EmployeeActive::create($newShift);
             }
 
