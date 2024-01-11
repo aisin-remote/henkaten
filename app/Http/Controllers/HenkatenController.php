7c5903363afedf0f5e756620c2daf137f3e2c887
AIIA\fabian <fabian@aiia.co.id>
2024-01-11 16:26:53 +0700

    push notif wa

--- app/Http/Controllers/HenkatenController.php
+++ app/Http/Controllers/HenkatenController.php
@@ -150,6 +150,27 @@ class HenkatenController extends Controller
                 'henkaten_id' => $henkaten->id,
             ]);
 
+            // send WA notif
+            $token = 'v2n49drKeWNoRDN4jgqcdsR8a6bcochcmk6YphL6vLcCpRZdV1';
+            $phone = '081280613890';
+            $message = sprintf("---- ``` AIIA HENKATEN ALERT ``` ---- %c%cLINE                      : $line->name %cLINE STATUS       : $request->status %c4M                         : $request->type %cCategory               : $request->category %cABNORMALITY    : $request->abnormality %cTROUBLESHOOT  :  TBC  %cPIC                         : $pivot %c%c ---------------- BY AISIN BISA ----------------", 10, 10, 10, 10, 10, 10, 10,10,10,10);
+            
+            $curl = curl_init();
+            curl_setopt_array($curl, [
+                CURLOPT_URL => 'https://app.ruangwa.id/api/send_message',
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => '',
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 0,
+                CURLOPT_FOLLOWLOCATION => true,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => 'POST',
+                CURLOPT_POSTFIELDS => 'token=' . $token . '&number=' . $phone . '&message=' . $message,
+            ]);
+
+            $response = curl_exec($curl);
+            curl_close($curl);
+
             DB::commit();
             return redirect()
                 ->back()
--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -128,7 +128,8 @@
                                         class="rounded-circle" width="80" height="80" alt="" />
                                     <div class="ms-3">
                                         <h5 class="mb-1 fs-3">{{ auth()->user()->name }}</h5>
-                                        <span class="mb-2 mt-2 d-block badge bg-light-info text-info">Leader</span>
+                                        <span
+                                            class="mb-2 mt-2 d-block badge bg-light-info text-info">{{ auth()->user()->role }}</span>
                                         <p class="mb-0 d-flex text-dark align-items-center gap-2">
                                             <i class="ti ti-mail fs-4"></i> {{ auth()->user()->npk }}
                                         </p>
