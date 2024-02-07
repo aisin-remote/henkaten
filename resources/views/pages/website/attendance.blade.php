aa7aa163f01f95634e8d66dc8558764376120027
AIIA\fabian <fabian@aiia.co.id>
2024-02-07 11:52:08 +0700

    update ava

--- resources/views/pages/website/attendance.blade.php
+++ resources/views/pages/website/attendance.blade.php
@@ -52,7 +52,7 @@
 
                                         @endphp
                                         <div class="d-flex align-items-center justify-content-between mt-4">
-                                            <div class="d-flex">
+                                            <div class="d-flex gap-2">
                                                 <img src="{{ asset('uploads/doc/' . $employee->employee->photo) }}"
                                                     class="rounded-1 img-fluid" width="40">
                                                 <div>
